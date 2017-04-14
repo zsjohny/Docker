2edefc77e65262d72d09573e5fdc7bd1217ae0ac
johny.zheng <zs.johny@163.com>
2017-04-14 17:15:45 +0800

    Update install.sh

--- docker_trick/install.sh
+++ docker_trick/install.sh
@@ -22,7 +22,7 @@ sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_V
 
 # Install docker-cleanup command
 cd /tmp
-git clone https://gist.github.com/76b450a0c986e576e98b.git
-cd 76b450a0c986e576e98b
+# git clone https://gist.github.com/76b450a0c986e576e98b.git
+# cd 76b450a0c986e576e98b
 sudo mv docker-cleanup /usr/local/bin/docker-cleanup
 sudo chmod +x /usr/local/bin/docker-cleanup
